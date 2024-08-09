3a0c953511b1c3c08f3b498f501f52511cd75dcc
abdul-mohsen <abdh357@gmail.com>
2024-08-09 17:26:58 +0300

    pls works

--- pkg/handlers/getCarPartDetail.go
+++ pkg/handlers/getCarPartDetail.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"ifritah/web-service-gin/pkg/model"
@@ -12,15 +13,16 @@ import (
 func (h * handler) GetCarPartDetail(c *gin.Context) {
   
   rows, err := h.DB.Query("SELECT * FROM user")
-  var name = "hi";
   if err != nil {
-    fmt.Errorf("hahahah %q: %v", name, err)
+    log.Fatal(err)
+    return 
   }
   var users []model.User
   for rows.Next() {
     var user model.User
     if err := rows.Scan(&user.ID, &user.Username, &user.Password); err != nil {
-      fmt.Println("albumsByArtist %q: %v", name, err)
+      log.Fatal(err)
+      return 
     }
     fmt.Println(user);
     users = append(users, user)

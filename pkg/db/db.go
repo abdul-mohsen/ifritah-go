0337426e8222179911b18682e3d576a1cff56f01
abdul-mohsen <abdh357@gmail.com>
2024-08-09 10:02:49 +0300

    init ifritah

---
+++ pkg/db/db.go
@@ -0,0 +1,42 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+func Connect() *sql.DB {
+  // Capture connection properties.
+  cfg := mysql.Config{
+    User:   os.Getenv("USER"),
+    Passwd: os.Getenv("PASSWORD"),
+    Net:    "tcp",
+    Addr:   os.Getenv("HOST"),
+    DBName: os.Getenv("DBNAME"),
+    AllowNativePasswords: true,
+  }
+  // Get a database handle.
+  var db *sql.DB
+  var err error
+  db, err = sql.Open("mysql", cfg.FormatDSN())
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  pingErr := db.Ping()
+  if pingErr != nil {
+    log.Fatal(pingErr)
+  }
+  fmt.Println("Connected!")
+  return db
+
+}
+
+func CloseConnection(db *sql.DB) {
+  defer db.Close()
+}
+
---
+++ pkg/handlers/getCarPartDetail.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"ifritah/web-service-gin/pkg/model"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (h * handler) GetCarPartDetail(c *gin.Context) {
+  
+  rows, err := h.DB.Query("SELECT * FROM user")
+  var name = "hi";
+  if err != nil {
+    fmt.Errorf("hahahah %q: %v", name, err)
+  }
+  defer rows.Close()
+  var users []model.User
+  for rows.Next() {
+    var user model.User
+    if err := rows.Scan(&user.ID, &user.Username, &user.Password); err != nil {
+      fmt.Println("albumsByArtist %q: %v", name, err)
+    }
+    fmt.Println(user);
+    users = append(users, user)
+  }
+  c.IndentedJSON(http.StatusOK, users)
+
+}

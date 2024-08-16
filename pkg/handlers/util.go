2e3e0dd6a830e47b9312732d902a9f3f7ce7f878
abdul-mohsen <abdh357@gmail.com>
2024-08-16 17:08:33 +0300

    plz work

--- pkg/handlers/util.go
+++ pkg/handlers/util.go
@@ -1,16 +1,21 @@
 package handlers
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
 func getCol(item interface{}) []interface{} {
 
-    a := reflect.ValueOf(&item).Elem().Elem()
-    numCols := a.NumField()
-    columns := make([]interface{}, numCols)
-    for i := 0; i < numCols; i++ {
-      field := a.Field(i)
-      columns[i] = field.Addr().Interface()
-    }
+  a := reflect.ValueOf(&item).Elem().Elem()
+  fmt.Print(a)
+
+  numCols := a.NumField()
+  columns := make([]interface{}, numCols)
+  for i := 0; i < numCols; i++ {
+    field := a.Field(i)
+    columns[i] = field.Addr().Interface()
+  }
   return columns
 
 }

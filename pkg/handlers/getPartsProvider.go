2d63139540e1fc9f8a791973d2c8c865e8fbf9fa
abdul-mohsen <abdh357@gmail.com>
2024-08-17 11:45:53 +0300

    fix query

--- pkg/handlers/getPartsProvider.go
+++ pkg/handlers/getPartsProvider.go
@@ -12,7 +12,7 @@ import (
 
 func (h * handler) GetPartsProvider(c *gin.Context) {
   id := c.Param("company_id") 
-  rows, err := h.DB.Query("SELECT name, address, phone_number, number, vat_number FROM parts_provider where store_id = ? and is_deleted = TRUE", id)
+  rows, err := h.DB.Query("SELECT name, address, phone_number, number, vat_number FROM parts_provider where company_id = ? and is_deleted = TRUE", id)
 
   if err != nil {
     log.Fatal(err)

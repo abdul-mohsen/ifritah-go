db3de1a6649d24a2cdcdbd22b626ccf0a473a81c
abdul-mohsen <abdh357@gmail.com>
2024-08-23 21:14:45 +0300

    Added jwt suppoer for go

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module ifritah/web-service-gin
 go 1.21.11
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.10.0
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/joho/godotenv v1.5.1
@@ -21,6 +22,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
--- pkg/handlers/getPartsProvider.go
+++ pkg/handlers/getPartsProvider.go
@@ -1,20 +1,28 @@
 package handlers
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+  "fmt"
+  "log"
+  "net/http"
+  "github.com/dgrijalva/jwt-go"
+  "ifritah/web-service-gin/pkg/model"
 
-	"ifritah/web-service-gin/pkg/model"
-
-	"github.com/gin-gonic/gin"
+  "github.com/gin-gonic/gin"
 )
 
 func (h * handler) GetPartsProvider(c *gin.Context) {
-  id := c.Param("company_id") 
-  fmt.Println(c.Request)
-  fmt.Println(c.Request.Header)
-  fmt.Println(c.Request.Header.Get("Authorization"))
+
+  auth := c.Request.Header.Get("Authorization")
+  token, err := jwt.Parse(auth, func(token *jwt.Token) (interface{}, error) {
+    _, ok := token.Method.(*jwt.SigningMethodECDSA)
+    if !ok {
+      log.Fatal("fuck")
+    }
+    fmt.Println(ok)
+    return ok, nil
+  })
+  fmt.Println(token)
+  id := 1
   rows, err := h.DB.Query("SELECT name, address, phone_number, number, vat_number FROM parts_provider where company_id = ? and is_deleted = TRUE", id)
 
   if err != nil {
@@ -31,5 +39,9 @@ func (h * handler) GetPartsProvider(c *gin.Context) {
   }
   defer rows.Close()
   c.IndentedJSON(http.StatusOK, partsProviders)
+
+}
+
+func verifyJWT(endpointHandler func(writer http.ResponseWriter, request *http.Request)) http.HandlerFunc {
 
 }

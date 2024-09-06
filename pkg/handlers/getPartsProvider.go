f67960b4d301434ef6a161e8a7b9df6ddbd29846
abdul-mohsen <abdh357@gmail.com>
2024-09-06 16:15:29 +0300

    try another way

--- pkg/handlers/getPartsProvider.go
+++ pkg/handlers/getPartsProvider.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strings"
 
 	"github.com/dgrijalva/jwt-go"
 
@@ -14,30 +13,32 @@ import (
 )
 
 func verifyToken(tokenString string) (*jwt.Token, error) {
-	// Parse the token with the secret key
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		return os.Getenv("JWT_SECRET_KEY"), nil
-	})
+  // Parse the token with the secret key
+  fmt.Println(tokenString)
+  token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+    return os.Getenv("JWT_SECRET_KEY"), nil
+  })
+  fmt.Println(token)
 
-	// Check for verification errors
-	if err != nil {
-		return nil, err
-	}
+  // Check for verification errors
+  if err != nil {
+    return nil, err
+  }
 
-	// Check if the token is valid
-	if !token.Valid {
-		return nil, fmt.Errorf("invalid token")
-	}
+  // Check if the token is valid
+  if !token.Valid {
+    return nil, fmt.Errorf("invalid token")
+  }
 
-	// Return the verified token
-	return token, nil
+  // Return the verified token
+  return token, nil
 }
 
 func (h * handler) GetPartsProvider(c *gin.Context) {
 
   fullTokenString := c.Request.Header.Get("Authorization")
-  tokenString := strings.Split(fullTokenString, "Bearer ")[1]
-  fmt.Println(fullTokenString)
+  tokenString := fullTokenString
+  fmt.Println(tokenString)
   token, err := verifyToken(tokenString)
   if err != nil {
     log.Fatal(err)

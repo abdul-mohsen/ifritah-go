f449c95d2cb9ac9c3aeaef07bf5556187fbd93d4
abdul-mohsen <abdh357@gmail.com>
2024-09-07 04:53:21 +0300

    fix jwt parser and move to safe place

--- pkg/handlers/getPartsProvider.go
+++ pkg/handlers/getPartsProvider.go
@@ -5,48 +5,18 @@ import (
 	"ifritah/web-service-gin/pkg/model"
 	"log"
 	"net/http"
-	"os"
 	"strings"
-
-	"github.com/dgrijalva/jwt-go"
 
 	"github.com/gin-gonic/gin"
 )
 
-func verifyToken(tokenString string) (*jwt.Token, error) {
-  // Parse the token with the secret key
-  fmt.Println(tokenString)
-  token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-    key := os.Getenv("JWT_SECRET_KEY")
-    fmt.Println(key)
-    fmt.Println("_0")
-    return []byte(key), nil
-  })
-  fmt.Println(token)
-  fmt.Println("_1")
-
-  // Check for verification errors
-  if err != nil {
-    return nil, err
-  }
-  fmt.Println("_2")
-
-  // Check if the token is valid
-  if !token.Valid {
-    return nil, fmt.Errorf("invalid token")
-  }
-  fmt.Println("_3")
-
-  // Return the verified token
-  return token, nil
-}
 
 func (h * handler) GetPartsProvider(c *gin.Context) {
 
   fullTokenString := c.Request.Header.Get("Authorization")
   tokenString := strings.Split(fullTokenString, "Bearer ")[1]
   fmt.Println(tokenString)
-  token, err := verifyToken(tokenString)
+  token, err := VerifyToken(tokenString)
   if err != nil {
     log.Fatal(err)
   }
--- pkg/handlers/handler.go
+++ pkg/handlers/handler.go
@@ -1,6 +1,12 @@
 package handlers
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/dgrijalva/jwt-go"
+)
 
 type handler struct {
   DB *sql.DB
@@ -8,4 +14,30 @@ type handler struct {
 
 func New(db *sql.DB) handler {
   return handler{db}
+}
+
+
+func VerifyToken(tokenString string) (*jwt.Token, error) {
+  // Parse the token with the secret key
+  fmt.Println(tokenString)
+  token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+    key := os.Getenv("JWT_SECRET_KEY")
+    fmt.Println(key)
+    fmt.Println("_0")
+    return []byte(key), nil
+  })
+  fmt.Println(token)
+
+  // Check for verification errors
+  if err != nil {
+    return nil, err
+  }
+
+  // Check if the token is valid
+  if !token.Valid {
+    return nil, fmt.Errorf("invalid token")
+  }
+
+  // Return the verified token
+  return token, nil
 }

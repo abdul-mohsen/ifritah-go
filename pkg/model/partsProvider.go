217aa751861e11c0a9700a50574c351cb4453b9b
abdul-mohsen <abdh357@gmail.com>
2024-09-09 20:59:36 +0300

    parse the class to json

--- pkg/model/partsProvider.go
+++ pkg/model/partsProvider.go
@@ -3,10 +3,10 @@ package model
 type PartsProvider struct {
   Id int32 `json:"-"`
   Copany_id int32 `json:"-"`
-  Name string
-  Address string
-  PhoneNumber string
-  Number string
-  VatNumber string
-  IsDeleted bool
+  Name string `json:"name"`
+  Address string `json:"address"`
+  PhoneNumber string `json:"phone_number"`
+  Number string `json:"number"`
+  VatNumber string `json:"vat_number"`
+  IsDeleted bool `json:"is_deleted"`
 }

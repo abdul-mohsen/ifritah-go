9c43490b242ae3a6b4ffd77d7fb3556eca2e9b26
abdul-mohsen <abdh357@gmail.com>
2024-11-28 04:43:39 +0300

    fix login token

--- pkg/handlers/auth.go
+++ pkg/handlers/auth.go
@@ -13,117 +13,118 @@ import (
 )
 
 type RegisterRequest struct {
-  Username string `json:"username"`
-  Password string `json:"password"`
-  Address string `json:"address"`
-  PhoneNumber string `json:"phone_number"`
-  Email string `json:"email"`
-  CompanyID int `json:"company_id"`
+	Username    string `json:"username"`
+	Password    string `json:"password"`
+	Address     string `json:"address"`
+	PhoneNumber string `json:"phone_number"`
+	Email       string `json:"email"`
+	CompanyID   int    `json:"company_id"`
 }
 
-func (h * handler) Register(c *gin.Context) {
+func (h *handler) Register(c *gin.Context) {
 
-  var request RegisterRequest
-  if err := c.BindJSON(&request); err != nil {
-    log.Panic(err)
-  }
-  hashedPassword, err := bcrypt.GenerateFromPassword([]byte(request.Password), 10)
-  if err != nil {
-    log.Fatalf("Error hashing password: %v", err)
-  }
+	var request RegisterRequest
+	if err := c.BindJSON(&request); err != nil {
+		log.Panic(err)
+	}
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(request.Password), 10)
+	if err != nil {
+		log.Fatalf("Error hashing password: %v", err)
+	}
 
-  if _, err := h.DB.Exec("INSERT INTO user (company_id, username, password ) VALUES (?, ?, ?)", request.CompanyID, request.Username, hashedPassword); err != nil {
-    log.Panic(err)
-  }
+	if _, err := h.DB.Exec("INSERT INTO user (company_id, username, password ) VALUES (?, ?, ?)", request.CompanyID, request.Username, hashedPassword); err != nil {
+		log.Panic(err)
+	}
 
-  c.Status(http.StatusCreated)
+	c.Status(http.StatusCreated)
 }
-
 
 type JWTConfig struct {
-	AccessSecretKey     string
-	RefreshSecretKey    string
-	SigningMethod       string
-	AccessExpiration    time.Duration
-	RefreshExpiration   time.Duration
+	AccessSecretKey   string
+	RefreshSecretKey  string
+	SigningMethod     string
+	AccessExpiration  time.Duration
+	RefreshExpiration time.Duration
 }
 
 var JWTSettings = JWTConfig{
-	AccessSecretKey:     os.Getenv("ACCESS_SECRET_KEY"),
-	RefreshSecretKey:    os.Getenv("REFRESH_SECRET_KEY"),
-	SigningMethod:       "HS512",
-	AccessExpiration:    time.Minute * 15, // Access token expires in 15 minutes
-	RefreshExpiration:   time.Hour * 24 * 7, // Refresh token expires in 7 days
+	AccessSecretKey:   os.Getenv("ACCESS_SECRET_KEY"),
+	RefreshSecretKey:  os.Getenv("REFRESH_SECRET_KEY"),
+	SigningMethod:     "HS512",
+	AccessExpiration:  time.Minute * 15,   // Access token expires in 15 minutes
+	RefreshExpiration: time.Hour * 24 * 7, // Refresh token expires in 7 days
 }
 
 type LoginRequest struct {
-  Username string `json:"username"`
-  Password string `json:"password"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 func GenerateAccessToken(username string, userid int) (string, error) {
-  token := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
-    "username": username,
-    "userId": userid,
-    "exp":      time.Now().Add(JWTSettings.AccessExpiration).Unix(),
-  })
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
+		"username": username,
+		"userId":   userid,
+		"exp":      time.Now().Add(JWTSettings.AccessExpiration).Unix(),
+	})
 
-  return token.SignedString([]byte(JWTSettings.AccessSecretKey))
+	return token.SignedString([]byte(JWTSettings.AccessSecretKey))
 }
 
 func GenerateRefreshToken(username string, userid int) (string, error) {
-  token := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
-    "username": username,
-    "userId": userid,
-    "exp":      time.Now().Add(JWTSettings.RefreshExpiration).Unix(),
-  })
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
+		"aud":      "http://0.0.0.0:4194/hello",
+		"sub":      "Authentication",
+		"iss":      "softwaret",
+		"username": username,
+		"userId":   userid,
+		"exp":      time.Now().Add(JWTSettings.RefreshExpiration).Unix(),
+	})
 
-  return token.SignedString([]byte(JWTSettings.RefreshSecretKey))
+	return token.SignedString([]byte(JWTSettings.RefreshSecretKey))
 }
 
-func checkPassword(hashedPassword [] byte, password string) error {
-    return bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+func checkPassword(hashedPassword []byte, password string) error {
+	return bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
 }
 
-func (h * handler) Login(c *gin.Context) {
+func (h *handler) Login(c *gin.Context) {
 
-  var request LoginRequest 
-  if err := c.BindJSON(&request); err != nil {
-    log.Panic(err)
-  }
+	var request LoginRequest
+	if err := c.BindJSON(&request); err != nil {
+		log.Panic(err)
+	}
 
-  hashedPassword, err := bcrypt.GenerateFromPassword([]byte(request.Password), 10)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(request.Password), 10)
 
-  var id int
-  var password string
-  if err := h.DB.QueryRow("SELECT id, password FROM user where username = ? limit 1;", request.Username).Scan(&id, &password); err != nil {
-    log.Panic(err)
-  }
+	var id int
+	var password string
+	if err := h.DB.QueryRow("SELECT id, password FROM user where username = ? limit 1;", request.Username).Scan(&id, &password); err != nil {
+		log.Panic(err)
+	}
 
-  if err != nil {
-    log.Fatalf("Error hashing password: %v", err)
-  }
+	if err != nil {
+		log.Fatalf("Error hashing password: %v", err)
+	}
 
-  err = checkPassword(hashedPassword, password)
-  if err != nil {
-    fmt.Println("Invalid password")
-  }
+	err = checkPassword(hashedPassword, password)
+	if err != nil {
+		fmt.Println("Invalid password")
+	}
 
-  accessToken, err := GenerateAccessToken(request.Username, id)
-  if err != nil {
-    c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not generate access token"})
-    return
-  }
+	accessToken, err := GenerateAccessToken(request.Username, id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not generate access token"})
+		return
+	}
 
-  refreshToken, err := GenerateRefreshToken(request.Username, id)
-  if err != nil {
-    c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not generate refresh token"})
-    return
-  }
+	refreshToken, err := GenerateRefreshToken(request.Username, id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not generate refresh token"})
+		return
+	}
 
-  c.JSON(http.StatusOK, gin.H{
-    "access_token":  accessToken,
-    "refresh_token": refreshToken,
-  })
+	c.JSON(http.StatusOK, gin.H{
+		"access_token":  accessToken,
+		"refresh_token": refreshToken,
+	})
 }
-

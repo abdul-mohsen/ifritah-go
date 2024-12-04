5c5d69ebe34945e378960893a7795c98e96f95d9
abdul-mohsen <abdh357@gmail.com>
2024-12-04 13:06:08 +0300

    test in prod

--- pkg/model/partsProvider.go
+++ pkg/model/partsProvider.go
@@ -1,13 +1,13 @@
 package model
 
 type Supplier struct {
-	Id          int32  `json:"id"`
-	Copany_id   int32  `json:"-"`
-	Name        string `json:"name"`
-	Address     string `json:"address"`
-	PhoneNumber string `json:"phone_number"`
-	Number      string `json:"number"`
-	VatNumber   string `json:"vat_number"`
-	IsDeleted   bool   `json:"-"`
-	BankAccount string `json:"bank_account"`
+	Id          int32   `json:"id"`
+	Copany_id   int32   `json:"-"`
+	Name        string  `json:"name"`
+	Address     string  `json:"address"`
+	PhoneNumber string  `json:"phone_number"`
+	Number      string  `json:"number"`
+	VatNumber   string  `json:"vat_number"`
+	IsDeleted   bool    `json:"-"`
+	BankAccount *string `json:"bank_account"`
 }

7812f2c42ce4db6ff969ff464715ca7988dc0419
abdul-mohsen <abdh357@gmail.com>
2024-12-15 02:47:16 +0300

    save til hil comes

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func main() {
 		authorized.GET("notification", h.GetNotificationAll)
 		authorized.POST("bill/all", h.GetBills)
 		authorized.GET("stores/all", h.GetStores)
-		authorized.GET("part/type", cache.CachePage(store, time.Minute, h.GetPartType))
+		authorized.GET("part/type", cache.CachePage(store, time.Duration(time.Friday), h.GetPartType))
 		// router.GET(baseUrl + ":id", h.GetCarPartDetail)
 	}
 	router.POST(baseUrl+"register", h.Register)

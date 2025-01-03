263c2650c3fe21d48b9b7ba07837139b072c2721
abdul-mohsen <abdh357@gmail.com>
2025-01-03 10:44:02 +0300

    fuc

--- pkg/handlers/bill.go
+++ pkg/handlers/bill.go
@@ -184,7 +184,7 @@ func (h *handler) AddBill(c *gin.Context) {
 	squenceNumber := h.getNextSquenceNumber(userSession.id)
 
 	query := `
-	insert into bill (effective_date, payment_due_date, state, sub_total, discount, vat, store_id, sequence_number, merchent_id, maintenance_cost, note, userName, buyer_id, user_phone_number)
+	insert into bill (effective_date, payment_due_date, state, sub_total, discount, vat, store_id, sequence_number, merchant_id, maintenance_cost, note, userName, buyer_id, user_phone_number)
 	values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
 	`
 

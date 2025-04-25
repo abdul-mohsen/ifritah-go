6108a28acb2900bc3aa948fc84ba0a7938dc8b9c
abdul-mohsen <abdh357@gmail.com>
2025-04-25 17:25:49 +0300

    fix year hopefully

--- pkg/handlers/vin.go
+++ pkg/handlers/vin.go
@@ -9,8 +9,10 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/text/cases"
 )
 
 func (h *handler) SearchByVin(c *gin.Context) {
@@ -129,6 +131,7 @@ func (h *handler) saveRequest(vin string, body []byte) {
 func (h *handler) searchByVin(c *gin.Context) BaseModel {
 	body := h.searchByVinRaw(c)
 
+	var vin string = strings.ToUpper(c.Param("vin"))
 	var response VehicleResponse
 
 	if err := json.Unmarshal(body, &response); err != nil {
@@ -140,7 +143,7 @@ func (h *handler) searchByVin(c *gin.Context) BaseModel {
 			Vin:   *response.Data.Intro.VIN,
 			Make:  response.Data.Basic.Make,
 			Model: response.Data.Basic.Model,
-			Year:  response.Data.Basic.Year,
+			Year:  getYear(vin),
 		}
 
 		return model
@@ -151,15 +154,84 @@ func (h *handler) searchByVin(c *gin.Context) BaseModel {
 	if err := json.Unmarshal(body, &europeVehicle); err != nil {
 		log.Panic(err)
 	}
+
 	model := BaseModel{
 		Vin:   europeVehicle.VIN,
 		Make:  europeVehicle.Data.GeneralInformation.Make,
 		Model: europeVehicle.Data.GeneralInformation.Model,
-		Year:  europeVehicle.Data.GeneralInformation.ModelYear,
+		Year:  getYear(vin),
 	}
 
 	return model
 
+}
+
+func getYear(c string) string {
+	switch c {
+	case "1":
+		return "2001"
+	case "2":
+		return "2002"
+	case "3":
+		return "2003"
+	case "4":
+		return "2004"
+	case "5":
+		return "2005"
+	case "6":
+		return "2006"
+	case "7":
+		return "2007"
+	case "8":
+		return "2008"
+	case "9":
+		return "2009"
+	case "A":
+		return "2010"
+	case "B":
+		return "2011"
+	case "C":
+		return "2012"
+	case "D":
+		return "2013"
+	case "E":
+		return "2014"
+	case "F":
+		return "2015"
+	case "G":
+		return "2016"
+	case "H":
+		return "2017"
+	case "J":
+		return "2018"
+	case "K":
+		return "2019"
+	case "L":
+		return "2020"
+	case "M":
+		return "2021"
+	case "N":
+		return "2022"
+	case "P":
+		return "2023"
+	case "R":
+		return "2024"
+	case "S":
+		return "2025"
+	case "T":
+		return "2026"
+	case "V":
+		return "2027"
+	case "W":
+		return "2028"
+	case "X":
+		return "2029"
+	case "Y":
+		return "2030"
+	default:
+		return "error"
+		// ch:eck how to handle this case
+	}
 }
 
 func getBody(url string) ([]byte, error) {

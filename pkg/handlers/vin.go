7ca729c535dcb2e365f99f56cb69a83905cb0cdf
abdul-mohsen <abdh357@gmail.com>
2025-04-29 20:26:48 +0300

    try to escap this hell

--- pkg/handlers/vin.go
+++ pkg/handlers/vin.go
@@ -331,7 +331,7 @@ func (h *handler) getPartDetailsByVinQuery(model BaseModel, q string, page, page
 	where manuName like ? and (? = NULL or o.number like ?)
 	limit ? offset ?
 	`
-	rows, err := h.DB.Query(query, model.Model, model.Year, model.Year+"12", model.Year, model.Year+"00", model.Make, q, q+"%", pageSize, page)
+	rows, err := h.DB.Query(query, withQute(model.Model), model.Year, model.Year+"12", model.Year, model.Year+"00", model.Make, q, q+"%", pageSize, page)
 	if err != nil {
 		log.Panic(err)
 	}
@@ -381,7 +381,7 @@ func (h *handler) getPartByVinQuery(model BaseModel, q string, pageSize, page in
 	where match(manuName) against(? in boolean mode)
 	limit ? offset ?
 	`
-	rows, err := h.DB.Query(query, "+"+model.Model, year, year, year, year, "+"+q+"*", "*"+model.Make+"*", pageSize, page)
+	rows, err := h.DB.Query(query, "+"+withQute(model.Model), year, year, year, year, withQute(q)+"*", "*"+withQute(model.Make)+"*", pageSize, page)
 	if err != nil {
 		log.Panic(err)
 	}
@@ -539,7 +539,7 @@ func (h *handler) getAllPartByVinQuery(model BaseModel) []Part {
 	join oem_number o on o.articleId = a.legacyArticleId
 	where match(manuName) against(?)
 	`
-	rows, err := h.DB.Query(query, "+"+model.Model, year, year, year, year, model.Make)
+	rows, err := h.DB.Query(query, "+"+withQute(model.Model), year, year, year, year, withQute(model.Make))
 	if err != nil {
 		log.Panic(err)
 	}
@@ -558,4 +558,8 @@ func (h *handler) getAllPartByVinQuery(model BaseModel) []Part {
 
 	defer rows.Close()
 	return parts
+}
+
+func withQute(s string) string {
+	return '"' + s + '"'
 }

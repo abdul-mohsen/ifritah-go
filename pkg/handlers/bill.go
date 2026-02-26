059becaaacf921f0f198e3551b714f9de7afccdb
ssda <abdulmohsen.jawad.almajed@gmail.com>
2026-02-26 16:32:49 +0300

    fix: add another raw for vat

--- pkg/handlers/bill.go
+++ pkg/handlers/bill.go
@@ -725,6 +725,7 @@ func (h *handler) GetBillPDF(c *gin.Context) {
 				TotalColumn:                  "السعر شامل الضريبة",
 				TotalDiscount:                "إجمالي الخصم",
 				Footer:                       fmt.Sprintf(">>>>>>>>>>>>>> إغلاق الفاتورة %d <<<<<<<<<<<<<<<", bill.SequenceNumber),
+				Vat:                          fmt.Sprint("ضريبة القيمة المضافة (%d%)", 15),
 			},
 			Language: "ar",
 			Products: products,

b6aef25e8c67411ab962480a699f0eea31816b7d
ssda <abdulmohsen.jawad.almajed@gmail.com>
2026-03-02 21:16:14 +0300

    fix: please work

--- pkg/db/queries/bill.sql
+++ pkg/db/queries/bill.sql
@@ -7,6 +7,7 @@ SELECT * from(
 			SELECT bill.id as id, effective_date, payment_due_date, bill.state as state, discount, sequence_number, bill.user_phone_number, TRUE as bill_type, cn.state as credit_state, total, total_vat, total_before_vat
 			FROM bill_totals as bill
 			JOIN credit_note  cn on cn.bill_id = bill.id
+			WHERE bill.user_phone_number like ?  and state >= 0
 			UNION
-			SELECT bill.id as id, effective_date, payment_due_date, bill.state as state, discount, sequence_number, user_phone_number, TRUE as bill_type, 0 as credit_state, total, total_vat, total_before_vat from bill_totals as bill
-		) AS T WHERE bill.user_phone_number like ?  and state >= 0 ORDER BY id DESC LIMIT ? OFFSET ?;
+			SELECT bill.id as id, effective_date, payment_due_date, bill.state as state, discount, sequence_number, user_phone_number, TRUE as bill_type, 0 as credit_state, total, total_vat, total_before_vat from bill_totals as bill WHERE bill.user_phone_number like ?  and state >= 0
+		) AS T ORDER BY id DESC LIMIT ? OFFSET ?;

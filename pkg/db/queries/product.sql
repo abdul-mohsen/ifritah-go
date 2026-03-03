4130335323fd65475ff7b86f4dda508d4310bc33
ssda <abdulmohsen.jawad.almajed@gmail.com>
2026-03-04 02:39:58 +0300

    fix: order in product

--- pkg/db/queries/product.sql
+++ pkg/db/queries/product.sql
@@ -4,7 +4,7 @@ from user
 join store s on s.company_id = user.company_id
 join product p on p.store_id = s.id
 join car_part.oem_number o
-where user.id = ? and o.articleId = p.article_id;
+where user.id = ? and o.articleId = p.article_id order by p.id desc;
 
 -- name: GetBillProductByBillID :many
 select * from bill_product where bill_id = ?;

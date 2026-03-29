105f7cf41dd24a38e5b5d723458bfd328a4a2816
ssda <abdulmohsen.jawad.almajed@gmail.com>
2026-03-29 13:15:05 +0300

    fix: the product id not sent

--- pkg/db/queries/product.sql
+++ pkg/db/queries/product.sql
@@ -2,7 +2,7 @@
 select p.* from product p where p.id = ? and is_deleted = False;
 
 -- name: GetAllProduct :many
-select  p.article_id, p.price, p.quantity, p.cost_price, p.shelf_number
+select  p.id, p.article_id, p.price, p.quantity, p.cost_price, p.shelf_number
 from user
 join store s on s.company_id = user.company_id
 join product p on p.store_id = s.id
